688a550d1ad7314107ea8eb07e940b002fb9c7ed
hugo vannier <hugo.vannier@csilyon.fr>
2024-10-13 15:42:12 +0200

    WORLD FIN

---
+++ Project3/World.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include "Entity.h"
+#include "StaticObject.h"
+#include "BreakableObject.h"
+#include "Mob.h"
+#include "Player.h"
+#include "World.h"
+#include "Vector2.h"
+
+World::World() :existingMob(false) {
+
+}
+
+void World::ExistingMob() {
+	existingMob = false;
+	for (Entity* e : vEntity) {
+		if (dynamic_cast<Mob*>(e)) {
+			existingMob = true;
+			return;
+		}
+	}
+}
+
+void World::EraseDead() {
+	for (int i = 0; i < vEntity.size(); i++) {
+		if (dynamic_cast<Alive*>(vEntity[i]) != nullptr) {
+			if (dynamic_cast<Alive*>(vEntity[i])->Get_Cur_HP() <= 0) {
+				vEntity.erase(vEntity.begin() + i);
+			}
+		}
+	}
+}
+
+bool World::CheckEnd() {
+	for (Entity* e : vEntity) {
+		if (dynamic_cast<Alive*>(e) != nullptr) {
+			if (dynamic_cast<Player*>(e) == nullptr) {
+				if (dynamic_cast<Alive*>(e)->Get_Cur_HP() > 0) {
+					return false;
+				}
+			}
+		}
+	}
+	std::cout << "Simulation Finished !";
+	return true;
+}
+
+void World::Init() {
+	StaticObject* staticObject = new StaticObject(5.f, 4.f);
+	BreakableObject* breakableObject = new BreakableObject(2.f, 2.f, 1.f);
+	Mob* mob = new Mob(5.f, 5.f, 20.f, 0.f, 0.f, 1.f);
+	Player* player = new Player(0.f, 0.f, 10.f, 0.f, 0.f, 1.f);
+
+	vEntity.push_back(staticObject);
+	vEntity.push_back(breakableObject);
+	vEntity.push_back(mob);
+	vEntity.push_back(player);
+
+	ExistingMob();
+}
+
+float World::GetDistanceBetween(Entity* e1, Entity* e2) {
+
+	float distanceX = e2->Get_PosX() - e1->Get_PosX();
+	float distanceY = e2->Get_PosY() - e1->Get_PosY();
+
+	float distance = sqrt(distanceX * distanceX + distanceY * distanceY);
+	return distance;
+}
+
+void World::Step() {
+
+	std::cout << std::endl;
+	EraseDead();
+	ExistingMob();
+
+	for (Entity* e : vEntity) {
+
+		Mob* mob = dynamic_cast<Mob*>(e);
+		Player* player = dynamic_cast<Player*>(e);
+
+		if (mob) {
+			for (Entity* entity : vEntity) {
+				BreakableObject* breakableObject = dynamic_cast<BreakableObject*>(entity);
+				if (breakableObject) {
+					mob->Move(*breakableObject);
+					break;
+				}
+			}
+		}
+
+		if (player) {
+
+			for (Entity* entity : vEntity) {
+				if (existingMob) {
+					Mob* targetMob = dynamic_cast<Mob*>(entity);
+					if (targetMob) {
+						player->Move(*targetMob);
+						float distanceBetween = GetDistanceBetween(player, targetMob);
+						std::cout << distanceBetween << std::endl;
+						if (abs(distanceBetween) <= 1) {
+							player->Atk(targetMob, 10);
+							break;
+						}
+					}
+				}
+				else {
+					BreakableObject* targetBreakableObject = dynamic_cast<BreakableObject*>(entity);
+					if (targetBreakableObject) {
+						player->Move(*targetBreakableObject);
+						float distanceBetween = GetDistanceBetween(player, targetBreakableObject);
+						if (abs(distanceBetween) <= 1) {
+							player->Atk(targetBreakableObject, 10);
+							break;
+						}
+					}
+				}
+			}
+		}
+
+	}
+	std::cout << std::endl;
+
+}
